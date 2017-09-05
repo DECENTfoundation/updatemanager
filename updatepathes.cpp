b863572191a85e33f07e42415be5db19a743cc36
ghbpeter <ps@softinengines.com>
2017-09-05 11:29:44 +0200

    Compilation errors on MacOs

--- updatepathes.cpp
+++ updatepathes.cpp
@@ -12,13 +12,16 @@ CUpdatePathes::CUpdatePathes(void)
 	uint32_t numOfChars = 0;
    char updateDir[1024];
 	std::string updateDirShort;
+#if defined( _MSC_VER )
 	char* tempDir = getenv("TMP");
 	if(tempDir == NULL)
 		tempDir = getenv("TEMP");
 
 	if(tempDir == NULL)
 		tempDir = getenv("SystemDrive");
-
+#else
+   char* tempDir = getenv("TMP");
+#endif
 	updateDirShort = tempDir;
 	
 	memset(updateDir, 0, sizeof(updateDir));
